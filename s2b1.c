3cbd259cb4a680625eea280d14f4384c562c0b68
duydoan169 <hanh250470@gmail.com>
2024-11-19 22:15:00 +0700

    s2 bài 1

---
+++ s2b1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a; //so nguyen(4 bytes)
+    float b; //so thuc(4 bytes)
+    double c; //float nhung chinh xac hon gap doi(8 bytes)
+    char d; //chua 1 ky tu(1 byte)
+    return 0;
+}
