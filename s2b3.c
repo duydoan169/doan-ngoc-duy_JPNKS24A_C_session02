c67f93c196218242ca6f9b97d8eefba72d01af50
duydoan169 <hanh250470@gmail.com>
2024-11-20 21:18:35 +0700

    s2 bài 3

---
+++ s2b3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(){
+    int a = 6, b = 7, tong, hieu, tich, thuong;
+    tong=a+b;
+    hieu=a-b;
+    tich=a*b;
+    thuong=a/b;
+    printf("tong la %d \n", tong);
+    printf("hieu la %d\n", hieu);
+    printf("tich la %d\n", tich);
+    printf("thuong la %d\n", thuong);
+
+
+    return 0;
+ }
