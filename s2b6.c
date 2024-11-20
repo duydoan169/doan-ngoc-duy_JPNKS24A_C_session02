4588afad0e4e657859aa177762d1565573dabe85
duydoan169 <hanh250470@gmail.com>
2024-11-20 21:39:38 +0700

    s2 bài 6

---
+++ s2b6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+    float banKinh=3, PI=3.14, chuVi, dienTich;
+    chuVi=2*PI*banKinh;
+    dienTich=PI*banKinh*banKinh;
+    printf("dien tich la %.2f \n", dienTich);
+    printf("Chu vi la %.2f\n", chuVi);
+    return 0;
+ }
