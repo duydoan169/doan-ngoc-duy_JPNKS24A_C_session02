95712eb82b08874469317689b544b6d7a9ebc1d2
duydoan169 <hanh250470@gmail.com>
2024-11-20 21:31:30 +0700

    s2 bài 5

---
+++ s2b5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+    int dai=6, rong=4, chuVi, dienTich;
+    chuVi=(dai+rong)*2;
+    dienTich=dai*rong;
+    printf("dien tich la %d \n", dienTich);
+    printf("Chu vi la %d\n", chuVi);
+    return 0;
+ }
