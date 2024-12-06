ec8ed517447891c9ca324e7d33f72a84b8a6f205
duydoan169 <hanh250470@gmail.com>
2024-12-06 08:28:35 +0700

    s2b1.c

--- s2b1.c
+++ s2b1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int a; //so nguyen(4 bytes)
+    int a; /*so nguyen(4 bytes)*/
     float b; //so thuc(4 bytes)
     double c; //float nhung chinh xac hon gap doi(8 bytes)
     char d; //chua 1 ky tu(1 byte)
